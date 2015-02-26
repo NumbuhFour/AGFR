7594ff980687c3acc258900334a603b7b3c5c92f
fad4470 <fad4470@rit.edu>
2015-02-26 16:32:53 -0500

    Moved corner damage calc to new SwordHitDamage HitDamage should be just for when the one sprite contacts something

--- Assets/Scripts/Game/Sprites/HitDamage.cs
+++ Assets/Scripts/Game/Sprites/HitDamage.cs
@@ -8,38 +8,20 @@ public class HitDamage : MonoBehaviour {
 	public int damage = 1;
 	public int knockback = 1;
 	
-	private Entity sprite;
-	private EntityLayer entLayer;
-	private Entity owner;
-	private FaceDirection fd;
-	
-	private bool hit = false;
-	private bool corner = false;
-	private Vector3 cornerCalc;
+	protected Entity sprite;
+	protected EntityLayer entLayer;
+	protected Entity owner;
+	protected FaceDirection fd;
+	protected bool hit;
 	// Use this for initialization
-	void Start () {
+	public virtual void Start () {
 		sprite = this.GetComponent<Entity>();
 		entLayer = GameObject.FindGameObjectWithTag("EntityLayer").GetComponent<EntityLayer>();
 		fd = gameObject.GetComponent<FaceDirection>();
-		
-		
-		bool flipped = false;
-		FlipSpriteByLastDirection flippy = GetComponent<FlipSpriteByLastDirection>();
-		if(flippy){
-			flipped = flippy.IsFlipped;
-		}
-		int dir = Direction.ConvertToIndex(fd.direction);
-		Vector3 findLastSwing = this.sprite.loc + Direction.ConvertToVector(dir+2)/* Back */ + Direction.ConvertToVector(dir+ (flipped?-1:1));
-		cornerCalc = this.sprite.loc + Direction.ConvertToVector(dir+ (flipped?-1:1));
-		
-		Entity otherSwing = sprite.entlayer.GetEntityAt(findLastSwing);
-		if(otherSwing && otherSwing.Name() == sprite.Name() && otherSwing.GetComponent<HitDamage>().owner == this.owner){ //Sword was found adjacent-ish
-			corner = true;
-		}
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	public virtual void Update () {
 		//if(!hit){
 		Entity target = entLayer.GetEntityAt(sprite.loc);
 		if(target && target != owner){
@@ -52,23 +34,11 @@ public class HitDamage : MonoBehaviour {
 					target.Knock(fd.direction, 1);
 			}
 		}
-		if(corner){
-			Entity targetCorner = entLayer.GetEntityAt(cornerCalc);
-			if(targetCorner && targetCorner != owner){
-				hit = true;
-				HealthTracker health = targetCorner.GetComponent<HealthTracker>();
-				if(!health) return;
-				health.TakeDamage(damage,sprite);
-				if(knockback > 0){
-					if(fd)
-						targetCorner.Knock(fd.direction, 1);
-				}
-			}
-		}
-		//}
 	}
 	
 	void SetOwner(Entity e){ //Called by SendMessage, sets creator of the swing
 		owner = e;
 	}
+	
+	public Entity Owner{ get { return owner; } }
 }
---
+++ Assets/Scripts/Game/Sprites/SwordHitDamage.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("Scripts/Game/Sprites/Sword Hit Damage")]
+[RequireComponent(typeof(Entity))]
+public class SwordHitDamage : HitDamage {
+	
+	private bool corner = false;
+	private Vector3 cornerCalc;
+	// Use this for initialization
+	public override void Start () {
+		base.Start();
+		
+		bool flipped = false;
+		FlipSpriteByLastDirection flippy = GetComponent<FlipSpriteByLastDirection>();
+		if(flippy){
+			flipped = flippy.IsFlipped;
+		}
+		int dir = Direction.ConvertToIndex(fd.direction);
+		Vector3 findLastSwing = this.sprite.loc + Direction.ConvertToVector(dir+2)/* Back */ + Direction.ConvertToVector(dir+ (flipped?-1:1));
+		cornerCalc = this.sprite.loc + Direction.ConvertToVector(dir+ (flipped?-1:1));
+		
+		Entity otherSwing = sprite.entlayer.GetEntityAt(findLastSwing);
+		if(otherSwing && otherSwing.Name() == sprite.Name() && otherSwing.GetComponent<HitDamage>().Owner == this.Owner){ //Sword was found adjacent-ish
+			corner = true;
+		}
+	}
+	
+	// Update is called once per frame
+	public override void Update () {
+		base.Update();
+		if(corner){
+			Entity targetCorner = entLayer.GetEntityAt(cornerCalc);
+			if(targetCorner && targetCorner != owner){
+				hit = true;
+				HealthTracker health = targetCorner.GetComponent<HealthTracker>();
+				if(!health) return;
+				health.TakeDamage(damage,sprite);
+				if(knockback > 0){
+					if(fd)
+						targetCorner.Knock(fd.direction, 1);
+				}
+			}
+		}
+	}
+}
